c4d395d0a6e1adc57225da32ecfe5c51e97ff9f5
Yaroslava <yyuf@mail.com>
2020-11-08 18:20:22 +0300

    hello

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-
+#include <string.h>
 int main(int argc, char **argv)
 {
-printf("Hello world!\n");
+char name[255];
+printf("Enter your name: ");
+printf("length = %d\n", strlen(name)); /* debug line */
+name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+printf("Hello %s!\n", name);
 return 0;
 }
